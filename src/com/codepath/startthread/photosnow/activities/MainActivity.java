7c2f444a0364e9d768ec4cc423b12e84b241f71b
Prakash Joshi <pjoshi@twitter.com>
2014-09-14 14:33:41 -0700

    Added pull to refresh

--- src/com/codepath/startthread/photosnow/activities/MainActivity.java
+++ src/com/codepath/startthread/photosnow/activities/MainActivity.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.widget.ListView;
 
@@ -20,46 +21,67 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
-	
+
 	public static final String CLIENT_ID = "3aebcb12934049968494ecf5fafa8c36";
-	
+
+	private SwipeRefreshLayout mSwipeContainer;
+
 	private ArrayList<InstagramPhoto> mPhotos = new ArrayList<InstagramPhoto>();
 	private InstagramPhotosAdapter mPhotosAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mPhotosAdapter = new InstagramPhotosAdapter(this, mPhotos);
-        
-        final ListView lvTimeline = (ListView) findViewById(R.id.lvTimeline);
-        lvTimeline.setAdapter(mPhotosAdapter);
-        
-        fetchPopularPhotos();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mPhotosAdapter = new InstagramPhotosAdapter(this, mPhotos);
+
+		final ListView lvTimeline = (ListView) findViewById(R.id.lvTimeline);
+		lvTimeline.setAdapter(mPhotosAdapter);
+
+		fetchPopularPhotos();
+
+		mSwipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+		mSwipeContainer.setColorSchemeResources(
+				android.R.color.holo_green_light,
+				android.R.color.holo_orange_light,
+				android.R.color.holo_blue_bright,				
+				android.R.color.holo_red_light);
+		
+		mSwipeContainer
+				.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+					@Override
+					public void onRefresh() {
+						fetchPopularPhotos();
+					}
+				});
+	}
 
 	private void fetchPopularPhotos() {
-		
+
 		// setup endpoint
-		final String popularUrl = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
-		
+		final String popularUrl = "https://api.instagram.com/v1/media/popular?client_id="
+				+ CLIENT_ID;
+
 		// network request
 		Log.d(TAG, "requesting popular photos");
 		AsyncHttpClient client = new AsyncHttpClient();
 		client.get(popularUrl, new JsonHttpResponseHandler() {
 
 			@Override
-			public void onFailure(int statusCode, Header[] headers, String response, Throwable e) {
+			public void onFailure(int statusCode, Header[] headers,
+					String response, Throwable e) {
 				Log.e(TAG, "failed to fetch data", e);
 			}
 
 			@Override
-			public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+			public void onSuccess(int statusCode, Header[] headers,
+					JSONObject response) {
 				Log.i(TAG, "successfully received response");
-				
+
+				mSwipeContainer.setRefreshing(false);
 				JSONArray photosJSON;
-				
+
 				try {
 					// data -> [i] -> user -> username
 					// data -> [i] -> user -> profile_picture
@@ -67,44 +89,52 @@ public class MainActivity extends Activity {
 					// data -> [i] -> likes -> count
 					// data -> [i] -> images -> standard_resolution -> url
 					// data -> [i] -> images -> standard_resolution -> height
-				
+
 					photosJSON = response.getJSONArray("data");
-					
+
 					mPhotos.clear();
-					
-					for (int i=0; i<photosJSON.length(); i++) {
+
+					for (int i = 0; i < photosJSON.length(); i++) {
 						JSONObject photoJSON = photosJSON.getJSONObject(i);
 						InstagramPhoto photo = new InstagramPhoto();
-						
-						photo.username = photoJSON.getJSONObject("user").getString("username");
-						photo.profilePicUrl = photoJSON.getJSONObject("user").getString("profile_picture");
-						
-						//JSONObject captionJSON = photoJSON.getJSONObject("caption");
+
+						photo.username = photoJSON.getJSONObject("user")
+								.getString("username");
+						photo.profilePicUrl = photoJSON.getJSONObject("user")
+								.getString("profile_picture");
+
+						// JSONObject captionJSON =
+						// photoJSON.getJSONObject("caption");
 						if (!photoJSON.isNull("caption")) {
-							photo.caption = photoJSON.getJSONObject("caption").getString("text");
+							photo.caption = photoJSON.getJSONObject("caption")
+									.getString("text");
 						}
-						
+
 						JSONObject likesJSON = photoJSON.getJSONObject("likes");
 						if (likesJSON != null) {
 							photo.likesCount = likesJSON.getInt("count");
 						}
-						
-						photo.url = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
-						photo.height = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
-						
+
+						photo.url = photoJSON.getJSONObject("images")
+								.getJSONObject("standard_resolution")
+								.getString("url");
+						photo.height = photoJSON.getJSONObject("images")
+								.getJSONObject("standard_resolution")
+								.getInt("height");
+
 						mPhotos.add(photo);
 					}
 					mPhotosAdapter.notifyDataSetChanged();
-					 
+
 				} catch (JSONException e) {
 					Log.w(TAG, "failed to parse JSON response", e);
 				}
 			}
-			
+
 		});
-		
+
 		// handle response
-		
+
 		// show data
 	}
 }
